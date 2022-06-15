7d36c8db7e3fe8f36b5abe9a8da6897089858ef9
Josué Osorio <89947764+josue16osorio@users.noreply.github.com>
2022-06-15 01:32:47 -0300

    Add files via upload

---
+++ calculadora.js
@@ -0,0 +1,23 @@
+const somar = require('./somar')
+const subtrair = require('./subtrair')
+const multiplicar = require('./multiplicar')
+const dividir = require('./dividir')
+a=1
+b=2
+
+/* Invocando Soma*/
+somar(a,b)
+console.log(somar(a,b))
+
+/* Invocando Subtrair*/
+subtrair(a,b)
+console.log(subtrair(a,b))
+
+/* Invocando multiplicar*/
+multiplicar(a,b)
+console.log(multiplicar(a,b))
+
+/* Invocando dividir*/
+dividir(a,b)
+console.log(dividir(a,b))
+
---
+++ dividir.js
@@ -0,0 +1,8 @@
+function dividir(a,b){
+    if(a==0 || b ==0){
+       return console.log("Não se pode dividir por 0")
+    }else{
+    return a/b
+}
+}
+module.exports=dividir;
